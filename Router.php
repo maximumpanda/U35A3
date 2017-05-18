5f38497ee3c386d4ba158ece97269a3cff8c8c1c
steven hirschmann <Maximumpanda@live.com>
2017-05-18 13:41:48 +0100

    testing error fix

--- Router.php
+++ Router.php
@@ -7,8 +7,8 @@
  */
 include_once 'RouterHelper.php';
 include_once 'RouteTable.php';
-include_once 'Controllers/MasterController.php';
-print $_SERVER['DOCUMENT_ROOT'];
+include_once 'MasterController.php';
+
 $uri = $_SERVER['REQUEST_URI'];
 if (strpos(end($uri), ".php") == false) $uri = $uri . "Index.php";
 $path = RouterHelper::GetPath($uri);
