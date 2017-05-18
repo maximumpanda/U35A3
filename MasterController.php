81ac5a757d2d56af3951ea864b0070e8af6a7030
steven hirschmann <Maximumpanda@live.com>
2017-05-18 18:13:46 +0100

    misc

--- MasterController.php
+++ MasterController.php
@@ -84,7 +84,7 @@ class MasterController{
         ];
         $parentListSize = count($parentList);
         Helper::Print($parentListSize);
-        for ($i = $parentListSize-1; $i > 0; $i--){
+        for ($i = $parentListSize-1; $i >= 0; $i--){
             Helper::Print($i . " " . $parentList[$i]);
             $newElement[$parentList[$i]] = [$element];
             $element = $newElement;
