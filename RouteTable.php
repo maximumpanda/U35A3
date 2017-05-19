5cc09b7e06cc6d4ae40551aa0c4beeec2b586709
steven hirschmann <Maximumpanda@live.com>
2017-05-19 13:46:25 +0100

    auto commit

--- RouteTable.php
+++ RouteTable.php
@@ -21,8 +21,7 @@ class RouteTable
         }
         if ($result == 0){
             array_push($path, self::$DefaultView);
-            Helper::PrintArray($path);
-            //header("location: " . Helper::GetBaseUrl() . "/" . implode("/", $path));
+            header("location: " . Helper::GetBaseUrl() . "/" . implode("/", $path));
         }
         return $result;
     }
