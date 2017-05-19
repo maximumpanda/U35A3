a17684a34b4766ac24e95b4a26d6dd617ba090dd
steven hirschmann <Maximumpanda@live.com>
2017-05-19 20:14:29 +0100

    auto commit

--- MasterController.php
+++ MasterController.php
@@ -50,10 +50,8 @@ class MasterController{
     public function FindMethod($class, $name){
         $methods = (new ReflectionClass($class))->getMethods(ReflectionMethod::IS_PUBLIC);
         foreach ($methods as $method){
-            Helper::Print($method->name);
-            Helper::Print(Helper::GetRequestMethod().$name);
             if (strtolower($method->name) == strtolower(Helper::GetRequestMethod().$name))
-                return $method;
+                return $method->name;
         }
         return false;
     }
