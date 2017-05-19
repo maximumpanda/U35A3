bd87b4d288b4a34309995867df8d3f63cf6e59ba
steven hirschmann <Maximumpanda@live.com>
2017-05-19 21:50:06 +0100

    auto commit

--- Views/Shared/Menu.php
+++ Views/Shared/Menu.php
@@ -1,9 +1,15 @@
 <div id="Menu">
     <?php
     include_once $_SERVER["DOCUMENT_ROOT"] . "/RouteTable.php";
+    $buttons = [];
     foreach (RouteTable::$Routes as $routeRoot => $branch){
         if (in_array($routeRoot, RouteTable::$HiddenBranches)) continue;
-        print '<a href="/'.$routeRoot . '" class="Element"">' . $routeRoot . '</a>';
+        array_push($buttons, $routeRoot);
     }
+    sort($buttons );
+    foreach ($buttons as $button) {
+        print '<a href="/' . $button . '" class="Element"">' . $button . '</a>';
+    }
+
     ?>
 </div>
