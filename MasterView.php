6d83442c44b822f5ff6bcf72e66b9b35fc2e5f49
steven hirschmann <Maximumpanda@live.com>
2017-05-19 20:52:05 +0100

    auto commit

--- MasterView.php
+++ MasterView.php
@@ -10,8 +10,7 @@ class MasterView
 {
 
     public static function GenerateView($path){
-        $view = self::ViewExists($path);
-        Helper::PrintArray($view);
+        $view = self::FindView($path);
         if ($view != false){
             Session::SetView($view);
             include_once $_SERVER["DOCUMENT_ROOT"] . "/Views/Shared/_Layout.php";
@@ -20,14 +19,13 @@ class MasterView
         RouteTable::ReDirectError(404);
     }
 
-    public static function ViewExists($path){
+    public static function FindView($path){
         $currentDir = $_SERVER['DOCUMENT_ROOT'] . "/Views/";;
-
+        $lastFound = "";
         foreach ($path as $item){
             $files = glob($currentDir ."*");
             $itemName = strtolower($currentDir) . strtolower($item);
             $found = false;
-            $lastFound = "";
             foreach ($files as $file){
                 if (strtolower($file) == $itemName || strtolower($file) == $itemName . ".html") {
                     $currentDir = $currentDir . basename($file). "/";
