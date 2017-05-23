63e2297c21814c77b624339582fbf66bf17b4e4b
Steven Hirschmann <stevenHirschmann14@gmail.com>
2017-05-23 17:19:18 +0100

    misc

---
+++ EntryPoint.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxim
+ * Date: 4/25/2017
+ * Time: 3:20 PM
+ */
+include_once $_SERVER['DOCUMENT_ROOT'] . "/Session.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/Helper.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/RouteTable.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/MasterController.php";
+$uri = $_SERVER['REQUEST_URI'];
+if ($uri == '/'){
+    $uri = Router::$DefaultPath;
+}
+$path = array_values(array_filter(explode("/", $uri)));
+new MasterController($path);
