4331b8965358ef708c23dc8f989a40c5a127b66a
Steven Hirschmann <stevenHirschmann14@gmail.com>
2017-05-29 12:25:09 +0100

    auto commit

--- RouteTable.php
+++ RouteTable.php
@@ -12,7 +12,7 @@ class RouteTable
     public static function ValidatePath($path){
         $result = RouteTable::CheckPathToDestination($path);
         if ($result == -1) {
-            Router::ReDirectError(404);
+            Router::ReDirectError(404, implode("/", $path));
         }
         if ($result == 0){
             Router::ReDirectIncomplete($path);
