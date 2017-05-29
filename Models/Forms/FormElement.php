9807aa4caee827a00fdf69187f918335ca455000
Steven Hirschmann <stevenHirschmann14@gmail.com>
2017-05-29 16:23:11 +0100

    auto commit

--- Models/Forms/FormElement.php
+++ Models/Forms/FormElement.php
@@ -31,7 +31,7 @@ class FormElement
         $this->Name = $object->Name;
         $this->_source = $object;
         $this->MaxLength = $object->Length;
-        if($object->KeyType == 1) $this->Disabled = true;
+        $this->Disabled = $object->KeyType == 2 ? true : false;
         $this->DefaultValue = $object->Value != null ? $object->Value : "";
         $this->InputType = FormElement::$InputTypes[$this->ParseInputType($object)];
     }
