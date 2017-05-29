7ccb6a6d28f47e4ae77280d2cc09a7d09102077b
Steven Hirschmann <stevenHirschmann14@gmail.com>
2017-05-29 12:08:54 +0100

    auto commit

--- Sql/Sql.php
+++ Sql/Sql.php
@@ -43,7 +43,7 @@ class Sql
     }
     public static function GetAllFromTable($table){
         $name = self::$_dbName . '.' . $table;
-        $query = "Select * From Addresses";
+        $query = "Select * From addresses";
         Helper::Print($name);
         return self::Query($query);
     }
