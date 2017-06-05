c4bb87b5dc20465cafc24b07f2d2f7085c6b1e24
steven hirschmann <Maximumpanda@live.com>
2017-06-06 00:05:06 +0100

    auto commit

--- Sql/SqlObject.php
+++ Sql/SqlObject.php
@@ -51,6 +51,7 @@ class SqlObject
             array_push($values, $field->Value);
         }
         $summary[$this->Fields['Id']->Value] = implode(" ,", $values);
+        Helper::PrintArray($summary);
         return $summary;
     }
 
