5355984824788484185a29ef1c2cb784efd78ef0
steven hirschmann <Maximumpanda@live.com>
2017-06-06 12:22:04 +0100

    auto commit

--- Views/Shared/Menu.php
+++ Views/Shared/Menu.php
@@ -3,7 +3,8 @@
     include_once $_SERVER["DOCUMENT_ROOT"] . "/RouteTable.php";
     $buttons = [];
     foreach (RouteTable::$Routes as $routeRoot => $branch){
-        if (in_array($routeRoot, RouteTable::$HiddenBranches) | $branch['AuthenticationLevel'] === 0) continue;
+        Helper::PrintArray($branch);
+        if (in_array($routeRoot, RouteTable::$HiddenBranches)) continue;
         array_push($buttons, $routeRoot);
     }
     sort($buttons);?>
