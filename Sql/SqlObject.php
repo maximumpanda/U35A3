434cf0fd107f89d2195543cc072adaab6ff3e234
steven hirschmann <Maximumpanda@live.com>
2017-06-12 11:42:57 +0100

    auto commit

--- Sql/SqlObject.php
+++ Sql/SqlObject.php
@@ -63,6 +63,9 @@ class SqlObject
             if ($field->KeyType == 2){
                 array_push($values, Sql::GetLinkedValues($field->TableName, $field->Value));
             }
+            if ($field->KeyType == 0){
+                array_push($values, [$field->Name=> $field->Value]);
+            }
         }
         $summary[$this->Fields['Id']->Value] = implode(", ", $values);
         return $summary;
