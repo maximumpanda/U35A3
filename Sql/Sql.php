6c4ecea1db43d613782518327b4a3a07fae43edc
steven hirschmann <Maximumpanda@live.com>
2017-06-12 12:45:38 +0100

    auto commit

--- Sql/Sql.php
+++ Sql/Sql.php
@@ -148,7 +148,6 @@ QUERY;
     }
 
     public static function GetLinkedValues($table, $where = ''){
-        Helper::Print($table);
         $model = self::GenerateModel($table, true);
         /** @var  $results SqlCollection */
         $query = self::BuildJoinStatement($model, $where);
