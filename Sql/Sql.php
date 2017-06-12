73051ff714520a9f7b3318524654d23ec7b57b12
steven hirschmann <Maximumpanda@live.com>
2017-06-12 13:30:18 +0100

    auto commit

--- Sql/Sql.php
+++ Sql/Sql.php
@@ -155,7 +155,7 @@ QUERY;
         $result = '';
         foreach ($res->Members as $key=>$value){
             foreach ($value->Fields as $field){
-                if ($field->Name !== 'Id')
+                if ($field->KeyType !== 2)
                     $result .= $field->Value . ', ';
             }
         }
