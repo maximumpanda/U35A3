f4a23ae0bc87990f90624f9d4e6e1a749a09f69d
steven hirschmann <Maximumpanda@live.com>
2017-06-12 08:49:29 +0100

    auto commit

--- Controllers/TrackerController.php
+++ Controllers/TrackerController.php
@@ -16,6 +16,7 @@ class TrackerController implements IController
         $query = "Select id, DeliveryTime, Weight, Status.Status AS Status From Packages ".
                  "LEFT JOIN Status On Packages.Status = Status.Id ".
                  "where Id =" . Session::$Bag['PackageId'];
+        Helper::Print($query);
         $res = Sql::Query($query);
         Helper::PrintArray($res);
         Session::$Bag['Status'] = "Success";
