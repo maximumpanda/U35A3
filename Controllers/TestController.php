93c5e3f9f1fffdb701db4c3a3bf2fbcd5c0acbb4
steven hirschmann <Maximumpanda@live.com>
2017-06-14 14:18:47 +0100

    auto commit

---
+++ Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: maxim
+ * Date: 6/14/2017
+ * Time: 2:17 PM
+ */
+class TestController implements IController
+{
+
+    public static function AuthenticationLevel()
+    {
+        return 0;
+    }
+
+    public static function GetIndex()
+    {
+    }
+}
